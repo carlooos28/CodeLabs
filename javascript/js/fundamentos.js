233ebd30daef29b74c40cdf1530532bb620d50a3
Carlos Moreno <carlooos_28@hotmail.com>
2019-09-18 16:55:02 -0500

    Refactor JS

--- javascript/js/fundamentos.js
+++ javascript/js/fundamentos.js
@@ -1,46 +1,37 @@
 // funciones
-
 function greeting(name) {
   return `Hello ${name} !`;
 }
 // alert(greeting("Carlos")); 
 
-
 // Arrow Functions
 
 /*
-
 let greeting = (name) => `Hello ${name} !`
 greeting("Carlos");
-
  */
 
 // Scope de las variables
 
 /*
-
 var name = "Carlos"; 
 function greeting() {
   return `Hello ${name} !`;
 }
 greeting();
-
  */
 
 
 /* 
-
 var name = "Carlos"; 
 function greeting(name) {
   return `Hello ${name} !`;
 }
 greeting("Fedesoft"); 
-
 */  
 
 
 /* 
-
 var name = "Carlos"; 
 function greeting() {
 var name = "Joel"; 
